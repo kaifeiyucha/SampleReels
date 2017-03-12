489b180f3b5e0d810d5877ce9281a1229ba4d423
yuzhuol1 <liyuzhuoc_code@163.com>
2017-03-12 21:48:50 +0800

    update coding ,make some repeated coding to a function:ButtonListener.

--- app/src/main/java/com/example/samplereels/MainActivity.java
+++ app/src/main/java/com/example/samplereels/MainActivity.java
@@ -13,44 +13,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_movie = (Button)findViewById(R.id.btn_movie);
-        btn_movie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "热门电影", Toast.LENGTH_SHORT).show();
-            }
-        });
+        Button  btn_listen = (Button)findViewById(R.id.btn_movie);
+        btn_listen.setOnClickListener(new ButtonListener());
+
 
         Button btn_shares = (Button)findViewById(R.id.btn_shares);
-        btn_shares.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "股票雄鹰", Toast.LENGTH_SHORT).show();
-            }
-        });
+        btn_shares.setOnClickListener(new ButtonListener());
+
 
         Button btn_reader = (Button)findViewById(R.id.btn_reader);
-        btn_reader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "XYZ 阅读器", Toast.LENGTH_SHORT).show();
-            }
-        });
+        btn_reader.setOnClickListener(new ButtonListener() );
 
         Button btn_news = (Button)findViewById(R.id.btn_news);
-        btn_news.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "最新闻", Toast.LENGTH_SHORT).show();
-            }
-        });
+        btn_news.setOnClickListener(new ButtonListener());
+
 
         Button btn_graduation = (Button)findViewById(R.id.btn_graduation);
-        btn_graduation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "毕业设计", Toast.LENGTH_SHORT).show();
-            }
-        });
+        btn_graduation.setOnClickListener(new ButtonListener());
     }
-}
+
+    public class ButtonListener implements View.OnClickListener{
+        @Override
+            public void onClick(View view) {
+                switch(view.getId()){
+                    case R.id.btn_movie:
+                        Toast.makeText(MainActivity.this, "热门电影", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.btn_news:
+                        Toast.makeText(MainActivity.this, "最新闻", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.btn_reader:
+                        Toast.makeText(MainActivity.this, "XYZ阅读器", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.btn_shares:
+                        Toast.makeText(MainActivity.this, "股票雄鹰", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.btn_graduation:
+                        Toast.makeText(MainActivity.this, "毕业设计", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
